2203546cb9432ef6624c3637611abd1169ba9200
Mika88 <mikaspivak@gmail.com>
2019-04-17 23:44:23 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   get 'posts/:id/post_data', to: 'posts#post_data'
+<<<<<<< HEAD
   resources :authors
+=======
+
+>>>>>>> 8c71a6a7506ebf30c06783a955299218cf8044b9
 end
